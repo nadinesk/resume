5af408a1c1c57dddfc002b24c9e9c410f06b0009
nadinesk <nadine.khattak@gmail.com>
2017-11-18 22:48:25 -0500

    added work stuff

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,8 +7,10 @@ const Profile = props => {
 		<div> 
 			<div> {profileObj.name} </div>
 			<div> {profileObj.label} </div>
-			<div> {profileObj.email} </div>
-			<div> {profileObj.website} </div>
+			<div><a href={`mailto:${profileObj.email}`}>{profileObj.email}</a></div>
+			<div><a href={profileObj.website}>website</a></div>
+			<div><a href={profileObj.profiles[0].url}>{profileObj.profiles[0].network}</a></div>
+			<div><a href={profileObj.profiles[1].url}>{profileObj.profiles[1].network}</a></div>
 			<div> {profileObj.summary} </div>
 		</div> 
 		)
