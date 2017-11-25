41ff092bcd54c2130d0e3a97ae196042e1292c66
nadinesk <nadine.khattak@gmail.com>
2017-11-25 07:20:29 -0500

    map profile objects

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Profile = props => {
 	const profileObj = props.profileData; 
-	console.log(profileObj.name)
+	const profiles_map = profileObj.profiles.map((prof) => (       
+            <div><a href={prof.url}>{prof.network}</a></div>
+        ))  	
 	return (
 		<div> 
 			<div> {profileObj.name} </div>
 			<div> {profileObj.label} </div>
 			<div><a href={`mailto:${profileObj.email}`}>{profileObj.email}</a></div>
 			<div><a href={profileObj.website}>website</a></div>
-			<div><a href={profileObj.profiles[0].url}>{profileObj.profiles[0].network}</a></div>
-			<div><a href={profileObj.profiles[1].url}>{profileObj.profiles[1].network}</a></div>
+			<div>{profiles_map}</div>			
 			<div> {profileObj.summary} </div>
 		</div> 
 		)
