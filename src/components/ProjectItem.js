6f2a02ce1a2c65557ec1e38754488a00574dcfaa
nadinesk <nadine.khattak@gmail.com>
2017-11-26 20:29:25 -0500

    added technical projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,23 +4,53 @@ import React from 'react';
 const ProjectItem = props => {
 
 	const projectObj = props.projectItemData;
-	
-	const projectGithub_map = projectObj.summary.map((gitlink) => (                   
-            <li>{gitlink}</li>
-        ))  	
+  let projectGit; 
+  let projectDem; 
+
+  const projectGithub_map = (projectObj.github.length > 1) ? (
+      projectGit = projectObj.github.map((gitlink, key) => (           
+              (key === 0) ? 
+              <span><a href={gitlink}>Frontend</a>, </span> 
+              : 
+              <span><a href={gitlink}>Backend</a> </span>         
+        ))      
+    ) : 
+  (
+     projectGit= projectObj.github.map((gitlink, key) => (                             
+              <span><a href={gitlink}>Github </a></span>  
+    ))
+    )
+  
+  
+const projectDemo_map = (projectObj.demo.length > 1) ? (
+      projectDem= projectObj.demo.map((demolink, key) => (           
+              (key === 0) ? 
+              <span><a href={demolink}>Package</a>, </span> 
+              : 
+              <span><a href={demolink}>Usage</a> </span>         
+        ))      
+    ) : 
+  (
+     projectDem= projectObj.demo.map((demolink, key) => (                             
+              <span><a href={demolink}>Demo</a></span>  
+    ))
+    )
     
     return (
         <div>
-          <h4>{props.workItemData.company}</h4>
-          <span><h5>{props.workItemData.position}, &nbsp;
-          {props.workItemData.startDate} - {props.workItemData.endDate}</h5> </span>       
+          <h4>{props.projectItemData.title}</h4>
+          <div>Code: {projectGithub_map} &nbsp;</div> 
+          <div>{projectDemo_map}</div>     
           
-          <ul> 
-           {workSummary_map}
-          </ul>
+          <div>
+           {props.projectItemData.description}
+          </div>
+          <div> 
+           {props.projectItemData.summary}
+          </div>
           
         </div>
     )
 };
 
-export default WorkItem;
+export default ProjectItem;
