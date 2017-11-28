a4b7d8da52a23b9155eef2d1d53dbf80b0ebd4ce
nadinesk <nadine.khattak@gmail.com>
2017-11-27 22:11:03 -0500

    fonts

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Profile = props => {
 	const profileObj = props.profileData; 
 	const profiles_map = profileObj.profiles.map((prof) => (       
-            <a href={prof.url}>{prof.network} </a>
-    ))  	
+           (prof.network === "LinkedIn") ? <a href={prof.url}><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a> : 
+      	<a href={prof.url}><i class="fa fa-github-alt fa-lg" aria-hidden="true"></i> </a> 
+      	))
 	return (
 		<div> 
 			<h1> {profileObj.name} </h1>			
-			<span><a href={`mailto:${profileObj.email}`}>{profileObj.email} </a>
-			<a href={profileObj.website}>Website </a>
+			<span><a href={`mailto:${profileObj.email}`}><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+			<a href={profileObj.website}><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
 			{profiles_map} </span> 
 			<div>
 				{profileObj.summary} 
